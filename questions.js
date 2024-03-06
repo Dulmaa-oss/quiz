4b4c1ed653b73a1b9b3f9d510e63898be1d9f18e
Dulmaa-oss <137365160+Dulmaa-oss@users.noreply.github.com>
2024-03-07 00:09:09 +0900

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,56 @@
+let questions = [
+  {
+    numb: 1,
+    question: "How many times a day does Bukhuu do that?",
+    answer: "3",
+    options: ["0", "1", "2", "3"],
+  },
+  {
+    numb: 2,
+    question: "Bukhuu's nickname?",
+    answer: "Uncle Bukhuu",
+    options: [
+      "Uncle Bukhuu",
+      "Half Man Half Amazing",
+      "Small Jumbuuhei",
+      "Scamm Bukhuu",
+    ],
+  },
+  {
+    numb: 3,
+    question: "What is the smallest part of a Bukhuu's body?",
+    answer: "jumbugu",
+    options: ["stomach", "jumbugu", "brain", "heart"],
+  },
+  {
+    numb: 4,
+    question: "What does Bukhuu do most during the day?",
+    answer: "fart",
+    options: ["study", "eat something", "fart", "sleep"],
+  },
+  {
+    numb: 5,
+    question: "What is Bukhuu's dream?",
+    answer: "Make Mongolia a good country",
+    options: [
+      "Become a Ninja",
+      "Make Mongolia a good country",
+      "Do that thing 10 times a day",
+      "Be handsome like Sukhbat",
+    ],
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
